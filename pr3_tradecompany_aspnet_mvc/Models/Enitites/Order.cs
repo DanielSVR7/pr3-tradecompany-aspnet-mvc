b5314b1019cbe1ce46648e12920dc5de65c6cd4a
Даниил Свириденко <sviridenko-02@mail.ru>
2024-06-12 01:38:40 +0400

    CRUD для Customers

---
+++ pr3_tradecompany_aspnet_mvc/Models/Enitites/Order.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace pr3_tradecompany_aspnet_mvc.Models.Enitites
+{
+    public class Order
+    {
+        [Column("id")]
+        public Guid Id { get; set; }
+
+        [Column("customer_id")]
+        public Customer? Customer { get; set; }
+
+        [Column("date")]
+        public DateTime Date { get; set; }
+
+        [Column("amount")]
+        public double Amount { get; set; }
+    }
+}
