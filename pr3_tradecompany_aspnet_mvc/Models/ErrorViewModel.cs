751b6382a80c94ac6f756b7cd35081f581a3ca32
Даниил Свириденко <sviridenko-02@mail.ru>
2024-06-11 21:57:36 +0400

    Добавьте файлы проекта.

---
+++ pr3_tradecompany_aspnet_mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pr3_tradecompany_aspnet_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
