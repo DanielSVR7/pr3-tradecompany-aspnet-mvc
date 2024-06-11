3e47983d2113a0d32ee054564b456972acd0345e
Даниил Свириденко <sviridenko-02@mail.ru>
2024-06-11 22:48:34 +0400

    Подключение БД

--- pr3_tradecompany_aspnet_mvc/Program.cs
+++ pr3_tradecompany_aspnet_mvc/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using pr3_tradecompany_aspnet_mvc.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,7 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<ApplicationDbContext>();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("TradeCompany")));
+
 
 var app = builder.Build();
 
